9a0f3452054ffc7627e758d59cd3ec62e370ad59
Chao,Dou <touchao123@126.com>
2024-02-16 21:24:21 +0800

    merge changes

--- someip-core/build-someip-core.sh
+++ someip-core/build-someip-core.sh
@@ -5,7 +5,7 @@ cd vsomeip
 git checkout 3.1.20.3
 rm -rf build && mkdir -p build && cd build
 cmake -D ENABLE_SIGNAL_HANDLING=1 -D CMAKE_INSTALL_PREFIX=/usr ..
-make && make install
+make -j && make install
 cd ../..
 
 # cd capicxx-core-runtime 
