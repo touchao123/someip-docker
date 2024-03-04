ff2db6a9ce120603039c2871514404cd4a42969a
Chao,Dou <touchao123@126.com>
2024-03-04 21:30:06 +0800

    Initialized soa-ecu

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,3 @@
 
 
-
-docker build -t chao123/someip-base  someip-base
-
-docker build -t chao123/someip-core  someip-core
-
-
-docker build -t chao123/pb-sip protoc-someip-core
-
-docker build -t chao123/someip-stack someip
+docker build -t chao123/soa-ecu soa-ecu
